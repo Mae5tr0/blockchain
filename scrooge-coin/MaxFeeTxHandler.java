1275c4e262c2abf9c4717426c2d8f220c53827c5
Mae5tr0 <i.kirill.maestro@gmail.com>
2018-01-05 14:16:56 +0700

    Finish TxHandler#isValidTx

---
+++ scrooge-coin/MaxFeeTxHandler.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class MaxFeeTxHandler {
+    /**
+     *  finds a set of transactions with maximum total transaction fees -- i.e. maximize the sum over
+     *  all transactions in the set of (sum of input values - sum of output values)).
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+//        ArrayList<Transaction> result = new ArrayList<>(possibleTxs.length);
+//        for (Transaction tx : possibleTxs) {
+//            if (isValidTx(tx)) {
+//                result.add(tx);
+//            }
+//        }
+//        return result.toArray(new Transaction[0]);
+        return new Transaction[0];
+    }
+}
